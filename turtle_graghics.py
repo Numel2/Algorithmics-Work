8252054fb055de5c7b9a14f1b9401fd4d6831e06
Numel2 <tra0171@unihigh.vic.au>
2024-08-23 13:50:48 +1000

    added more shapes

--- turtle_graghics.py
+++ turtle_graghics.py
@@ -1,13 +1,16 @@
 from turtle import *
 from time import sleep
+
 speed(0)
+
 
 def tanisha_shape():
     x = 200
     for i in range(20):
         forward(x)
         right(130)
-        x = x-10
+        x = x - 10
+
 
 def oliver_shape():
     penup()
@@ -19,11 +22,38 @@ def oliver_shape():
     pendown()
     circle(10)
     penup()
-    goto(-40,110)
+    goto(-40, 110)
     pendown()
     circle(10)
 
 
+def laila():
+    y = 150
+    for i in range(100):
+        forward(y)
+        left(170)
+        y -= 2
+
+
+def long_shape():
+    goto(-300, -300)
+    for i in range(20):
+        triangle()
+        square()
+        forward(240)
+        left(30)
+
+def triangle():
+    for i in range(3):
+        forward(100)
+        left(120)
+
+
+def square():
+    for i in range(4):
+        forward(150)
+        left(90)
+
 
 if __name__ == '__main__':
-    oliver_shape()
+    long_shape()
