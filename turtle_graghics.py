33eeeea4171deee3e2dba58c262476c6653f1fec
Numel2 <tra0171@unihigh.vic.au>
2024-08-23 13:28:12 +1000

    shape

---
+++ turtle_graghics.py
@@ -0,0 +1,13 @@
+from turtle import *
+speed(0)
+
+def tanisha_shape():
+    x = 200
+    for i in range(20):
+        forward(x)
+        right(130)
+        x = x-10
+
+
+if __name__ == '__main__':
+    tanisha_shape()
