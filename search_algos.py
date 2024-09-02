6ffb11f586a601cd267bb03bfebb0bd8ba7f1d04
Numel2 <tra0171@unihigh.vic.au>
2024-09-03 08:58:34 +1000

    added binary search

--- search_algos.py
+++ search_algos.py
@@ -12,5 +12,25 @@ def linear_search():
     print('not in list')
 
 
+def binary_search():
+    low = 0
+    high = len(nums) - 1
+    steps = 0
+
+    while low <= high:
+        middle = (low + high) // 2
+        middle_num = nums[middle]
+
+        steps += 1
+
+        if middle_num > query:
+            high = middle - 1
+        elif middle_num < query:
+            low = middle + 1
+        elif middle_num == query:
+            return print(f'query is at index {middle} and took {steps} steps')
+    print('not in list')
+
+
 if __name__ == '__main__':
-    linear_search()
+    binary_search()
