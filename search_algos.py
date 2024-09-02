5f99e4d70040aec6f5f312d97bcdf5e74384c449
Numel2 <tra0171@unihigh.vic.au>
2024-09-02 11:25:22 +1000

    added linear search

--- search_algos.py
+++ search_algos.py
@@ -1,2 +1,16 @@
 from time import sleep
 
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+query = 6
+
+
+def linear_search():
+    for num in range(len(nums)):
+        if nums[num] == query:
+            print('index is', num)
+            return
+    print('not in list')
+
+
+if __name__ == '__main__':
+    linear_search()
